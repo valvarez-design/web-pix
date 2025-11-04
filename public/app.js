d6a901a0358c452adf634e69661a7028d8f1c94c
Veyda Alvarez <va2601@nyu.edu>
2025-11-04 15:33:25 -0500

    Add theme selector functionality with profile pictures

--- public/app.js
+++ public/app.js
@@ -64,3 +64,46 @@ function displayMessage(message) {
     messageContainer.appendChild(messageElement);
 }
 
+// Theme selector functionality
+const themeButtons = document.querySelectorAll('.theme-btn');
+const profilePic = document.querySelector('.profile-pic');
+
+console.log('Theme buttons found:', themeButtons);
+console.log('Profile pic element:', profilePic);
+
+// Profile pictures for each theme
+const themeProfiles = {
+    '1': 'media/Background-Images/pink-pp.png',
+    '2': 'media/Background-Images/cartoon-pp.png',
+    '3': 'media/Background-Images/skull-pp.png',
+    '4': 'media/Background-Images/goth-pp.png',
+    '5': 'media/Background-Images/rainbow-pp.png'
+};
+
+console.log('Theme profiles object:', themeProfiles);
+
+themeButtons.forEach(button => {
+    console.log('Adding event listener to button:', button);
+    
+    button.addEventListener('click', () => {
+        console.log('Button clicked!');
+        
+        const themeNumber = button.dataset.theme;
+        console.log('Theme number selected:', themeNumber);
+        
+        // Change theme
+        document.body.setAttribute('data-theme', themeNumber);
+        console.log('Theme attribute set on body');
+        
+        // Change profile picture
+        if (themeProfiles[themeNumber]) {
+            console.log('Changing profile pic to:', themeProfiles[themeNumber]);
+            profilePic.src = themeProfiles[themeNumber];
+            console.log('Profile pic src updated');
+        } else {
+            console.log('No profile picture found for theme:', themeNumber);
+        }
+    });
+});
+
+console.log('All event listeners added!');
